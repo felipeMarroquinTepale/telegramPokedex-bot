aa26617bb5cbd155a88569d6e301231bfefd3c7d
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2022-06-23 19:07:42 -0500

    first commit

---
+++ pokedex.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+
+
+
+def pokedex(id_name):
+    print('Este metodo sera para consumir la api de pokedex y seleccionar algunas caracteristicas de los pokemones mediante un ID')
+
+
+if __name__== "__main__" :
+    pokedex('pikachu')
